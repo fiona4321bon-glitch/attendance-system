18e6325dcb8acb9b0fd946b66f7ba9e1f03367de
阿橘老師 <fiona4321bon@ttct.edu.tw>
2026-09-10 11:06:18 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "";
+const SUPABASE_URL = "https://liskwucqtzkxynuchnuc.supabase.co";
 
-const SUPABASE_KEY = "";
+const SUPABASE_KEY = "sb_publishable_FZCoueQpOqH093rRRAsRVA_bVYf-wON";
